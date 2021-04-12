8a61f7266618b5fa4bdb95b1563a5601fa5fa8c5
DDoSKingI <65786029+DDoSKingI@users.noreply.github.com>
2021-04-12 19:21:40 +0300

    Add files via upload

---
+++ enet2/CMakeFiles/enet.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/callbacks.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/callbacks.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/compress.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/compress.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/host.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/host.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/list.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/list.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/packet.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/packet.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/peer.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/peer.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/protocol.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/protocol.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/unix.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/unix.c.o"
+  "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/win32.c" "/data/data/com.termux/files/home/growtopiaserver/enet server test/enet2/CMakeFiles/enet.dir/win32.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAS_FCNTL=1"
+  "HAS_GETHOSTBYADDR_R=1"
+  "HAS_GETHOSTBYNAME_R=1"
+  "HAS_INET_NTOP=1"
+  "HAS_INET_PTON=1"
+  "HAS_MSGHDR_FLAGS=1"
+  "HAS_POLL=1"
+  "HAS_SOCKLEN_T=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
